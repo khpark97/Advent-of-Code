0e5b51692026f24790d32407a631ca352ae2d6a2
Kihyun Park <khpark970@gmail.com>
2025-01-15 19:08:40 -0800

    Day 22 part 1

---
+++ Day22.py
@@ -0,0 +1,24 @@
+## STARTER CODE
+file = open('Day22_data', 'r')
+data = file.read()
+lines = data.splitlines()
+
+# PART 1
+total = 0
+for buyer in [int(i) for i in lines]:
+    for i in range(2000):
+        prod = buyer * 64
+        buyer ^= prod
+        buyer %= 16777216
+        quot = buyer//32
+        buyer ^= quot
+        buyer %= 16777216
+        prod = buyer * 2048
+        buyer ^= prod
+        buyer %= 16777216
+    # print(buyer)
+    total += buyer
+
+print(f"Part 1: {total}")
+
+# PART 2
