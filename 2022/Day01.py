592f5f679c3132f56ef836776626361f7adf746a
Kihyun Park <khpark970@gmail.com>
2025-01-29 18:09:24 -0800

    2022 Day 1

--- 2022/Day01.py
+++ 2022/Day01.py
@@ -1,9 +1,23 @@
 ## STARTER CODE
-file = open('Day1_data.txt', 'r')
+file = open('2022/Day01_data.txt', 'r')
 data = file.read()
 lines = data.splitlines()
 
 # PART 1
+i = 0
+elves = []
+elf = 0
+while i < len(lines):
+    if lines[i] == '':
+        elves.append(elf)
+        elf = 0
+    else:
+        elf += int(lines[i])
+    i += 1
 
+print(f"Part 1: {max(elves)}")
 
 # PART 2
+import heapq
+
+print(f"Part 2: {sum(heapq.nlargest(3, elves))}")
