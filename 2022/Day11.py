9bb52bf225ece45fea48c96f03d21978c73c288d
Kihyun Park <khpark970@gmail.com>
2025-02-05 20:51:56 -0800

    2022 Day 11 part 1

---
+++ 2022/Day11.py
@@ -0,0 +1,93 @@
+## STARTER CODE
+file = open('2022/Day11_data.txt', 'r')
+data = file.read()
+lines = data.splitlines()
+
+# PART 1
+
+from collections import deque
+import heapq
+
+monkeys = {}
+i = 0
+while i < len(lines):
+    num = int(lines[i].split()[1][0])
+    items = deque([int(x) for x in lines[i+1][18:].split(', ')])
+    op = lines[i+2][13:].split()[-2:]
+    div = int(lines[i+3].split()[-1])
+    t = int(lines[i+4].split()[-1])
+    f = int(lines[i+5].split()[-1])
+    monkeys[num] = [items, op, div, t, f]
+    i += 7
+
+active = [0] * len(monkeys)
+
+def round():
+    for j in monkeys:
+        items, op, div, t, f = monkeys[j]
+        active[j] += len(items)
+        while items:
+            item = items.popleft()
+            o, val = op
+            if val == 'old':
+                val = item
+            if o == '+':
+                item += int(val)
+            else:
+                item *= int(val)
+            item //= 3
+            if item % div == 0:
+                monkeys[t][0].append(item)
+            else:
+                monkeys[f][0].append(item)
+
+for k in range(20):
+    round()
+    # for m in monkeys:
+    #     print(f"Monkey {m}: {monkeys[m][0]}")
+    
+business = heapq.nlargest(2, active)
+
+print(f"Part 1: {business[0] * business[1]}")
+
+# PART 2
+monkeys = {}
+i = 0
+while i < len(lines):
+    num = int(lines[i].split()[1][0])
+    items = deque([int(x) for x in lines[i+1][18:].split(', ')])
+    op = lines[i+2][13:].split()[-2:]
+    div = int(lines[i+3].split()[-1])
+    t = int(lines[i+4].split()[-1])
+    f = int(lines[i+5].split()[-1])
+    monkeys[num] = [items, op, div, t, f]
+    i += 7
+
+active = [0] * len(monkeys)
+
+def round():
+    for j in monkeys:
+        items, op, div, t, f = monkeys[j]
+        active[j] += len(items)
+        while items:
+            item = items.popleft()
+            o, val = op
+            if val == 'old':
+                val = item
+            if o == '+':
+                item += int(val)
+            else:
+                item *= int(val)
+            if item % div == 0:
+                monkeys[t][0].append(item)
+            else:
+                monkeys[f][0].append(item)
+
+for k in range(20):
+    round()
+    # for m in monkeys:
+    #     print(f"Monkey {m}: {monkeys[m][0]}")
+    
+business = heapq.nlargest(2, active)
+
+print(f"Part 2: {business[0] * business[1]}")
