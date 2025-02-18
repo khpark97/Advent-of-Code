c75f24c6f480a773e7601690c8caf6cda26730e2
Kihyun Park <khpark970@gmail.com>
2025-02-17 22:24:09 -0800

    2022 Day 15

---
+++ 2022/Day15.py
@@ -0,0 +1,35 @@
+## STARTER CODE
+file = open('2022/Day15_data.txt', 'r')
+data = file.read()
+lines = data.splitlines()
+
+# PART 1
+import re
+
+sensors, beacons = [], []
+for l in lines:
+    s, b = [re.findall(r'-?\d+', part) for part in l.split(':')]
+    sensors.append(tuple(int(i) for i in s))
+    beacons.append(tuple(int(i) for i in b))
+
+def mh(a, b):
+    return sum([abs(int(i) - int(j)) for i, j in zip(a, b)])
+
+target = 2000000
+positions = set()
+for i in range(len(sensors)):
+    s, b = sensors[i], beacons[i]
+    diff = mh(s, b) - abs(target - s[1])
+    if diff >= 0:
+        for j in range(s[0] - diff, s[0] + diff + 1):
+            pos = (j, target)
+            if pos not in beacons and pos not in sensors:
+                positions.add((j, target))
+
+print(f"Part 1: {len(positions)}")
+
+# PART 2
+
+
+
+print(f"Part 2: {len(positions)}")
