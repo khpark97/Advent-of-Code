c959865fc1bfa41d6ea20d770ef601addc2067ce
Kihyun Park <khpark970@gmail.com>
2025-03-13 18:52:37 -0700

    2015 Day 4

--- 2015/Day04.py
+++ 2015/Day04.py
@@ -5,12 +5,24 @@ lines = data.splitlines()
 
 # PART 1
 
+import hashlib
 
+secret_key = 'iwrupvqb'
 
-print(f"Part 1: {score}")
+parts = {'00000': float('inf'), '000000': float('inf')}
+
+for start in ['00000', '000000']:
+    i = 0
+    key = secret_key + str(i)
+    encoded_string = key.encode('utf-8')
+    while not hashlib.md5(encoded_string).hexdigest().startswith(start):
+        i += 1
+        key = secret_key + str(i)
+        encoded_string = key.encode('utf-8')
+    parts[start] = i
+
+print(f"Part 1: {parts['00000']}")
 
 # PART 2
 
-
-
-print(f"Part 2: {score}")
+print(f"Part 2: {parts['000000']}")
