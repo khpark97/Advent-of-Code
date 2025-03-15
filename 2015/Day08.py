0c11855a97bfee56b4774b156551bd47a22f693a
Kihyun Park <khpark970@gmail.com>
2025-03-15 12:18:21 -0700

    2015 Day 8

--- 2015/Day08.py
+++ 2015/Day08.py
@@ -5,12 +5,33 @@ lines = data.splitlines()
 
 # PART 1
 
+part1 = 0
+for l in lines:
+    escape = 0
+    i = 1
+    while i < len(l) - 1:
+        pair = l[i:i+2]
+        if pair == '\\\"' or pair == '\\\\':
+            escape += 1
+            i += 1
+        elif pair == '\\x':
+            escape += 1
+            i += 3
+        else:
+            escape += 1
+        i += 1
+    part1 += len(l) - escape
 
-
-print(f"Part 1: {score}")
+print(f"Part 1: {part1}")
 
 # PART 2
 
+part2 = 0
+for l in lines:
+    literal = 2
+    for char in l:
+        if char == '\"' or char == '\\': 
+            literal += 1
+    part2 += literal
 
-
-print(f"Part 2: {score}")
+print(f"Part 2: {part2}")
