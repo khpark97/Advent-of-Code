f41dcaacd4bdcb5367fdba5b0ac5832698aa21bd
Kihyun Park <khpark970@gmail.com>
2025-03-16 14:44:51 -0700

    2015 Day 9

--- 2015/Day09.py
+++ 2015/Day09.py
@@ -5,12 +5,29 @@ lines = data.splitlines()
 
 # PART 1
 
+from itertools import permutations
 
+edges = {}
+cities = set()
+for l in lines:
+    c1, _, c2, _, dist = l.split()
+    edges[(c1, c2)] = edges[(c2, c1)] = int(dist)
+    cities.add(c1)
+    cities.add(c2)
 
-print(f"Part 1: {score}")
+shortest = float('inf')
+longest = 0
+for p in permutations(cities):
+    dist = 0
+    for i in range(len(p) - 1):
+        pair = p[i], p[i+1]
+        if pair in edges:
+            dist += edges[pair]
+    shortest = min(shortest, dist)
+    longest = max(longest, dist)
+
+print(f"Part 1: {shortest}")
 
 # PART 2
 
-
-
-print(f"Part 2: {score}")
+print(f"Part 2: {longest}")
