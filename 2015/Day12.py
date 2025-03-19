cd8d665ba2f8d4df1b9bf29fa59ef48ae937d6ee
Kihyun Park <khpark970@gmail.com>
2025-03-18 20:21:13 -0700

    2015 Day 13

--- 2015/Day12.py
+++ 2015/Day12.py
@@ -1,15 +1,35 @@
 ## STARTER CODE
 file = open('2015/Day12_data.txt', 'r')
 data = file.read()
-lines = data.splitlines()
 
 # PART 1
 
-total = 0
+import re
+
+total = sum([int(x) for x in re.findall(r'-?\d+', data)])
 
 print(f"Part 1: {total}")
 
 # PART 2
 
+import json
+
+parsed = json.loads(data)
+
+def find(object):
+    if 'red' in json.dumps(object):
+        for sub in object:
+            print(sub)
+            find(sub)
+    
+find(parsed[0])
+
+# for obj in parsed[5:]:
+#     if 'red' in obj:
+#         print(obj)
+#         break
+#     print(len(obj), 'red' in json.dumps(obj), 'red' in obj)
+
+x = json.dumps(parsed[0][1][0], indent=4)
 
 print(f"Part 2: {total}")
--- 2015/Day13.py
+++ 2015/Day13.py
@@ -5,11 +5,35 @@ lines = data.splitlines()
 
 # PART 1
 
-total = 0
+from collections import defaultdict
+from itertools import permutations
 
-print(f"Part 1: {total}")
+network = defaultdict(int)
+people = set()
+
+for l in lines:
+    words = l.strip('.').split()
+    p1, _, change, val, *_, p2 = words
+    people.add(p1)
+    people.add(p2)
+    network[tuple(sorted((p1, p2)))] += int(val) if change == 'gain' else -int(val)
+
+def sit(n, p):
+    happiness = 0
+    for p in permutations(p):
+        curr = n[tuple(sorted((p[0], p[-1])))]
+        for i in range(len(p) - 1):
+            curr += n[tuple(sorted((p[i], p[i+1])))]
+        happiness = max(happiness, curr)
+
+    return happiness
+
+print(f"Part 1: {sit(network, people)}")
 
 # PART 2
 
+for p in people:
+    network[tuple(sorted((p, 'me')))] = 0
+people.add('me')
 
-print(f"Part 2: {total}")
+print(f"Part 2: {sit(network, people)}")
