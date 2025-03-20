f0d673c4578a4c8f60e13b4b2832174e196edca7
Kihyun Park <khpark970@gmail.com>
2025-03-20 12:58:54 -0700

    2015 Day 16

--- 2015/Day16.py
+++ 2015/Day16.py
@@ -5,11 +5,53 @@ lines = data.splitlines()
 
 # PART 1
 
-total = 0
+import re
 
-print(f"Part 1: {total}")
+sue = {
+    'children': 3,
+    'cats': 7,
+    'samoyeds': 2,
+    'pomeranians': 3,
+    'akitas': 0,
+    'vizslas': 0,
+    'goldfish': 5,
+    'trees': 3,
+    'cars': 2,
+    'perfumes': 1
+}
+
+p1, p2 = -1, -1
+for i in range(len(lines)):
+    l = lines[i]
+    items = [item[:-1] for item in [l.split()[k] for k in [2,4,6]]]
+    aunt, *count = [int(x) for x in re.findall(r'\d+', l)]
+    
+    match_p1, match_p2 = 0, 0
+
+    for c in range(len(items)):
+        item = items[c]
+        if item in ['cats', 'trees']:
+            if sue[item] < count[c]:
+                match_p2 += 1
+            if sue[item] == count[c]:
+                match_p1 += 1
+        elif item in ['pomeranians', 'goldfish']:
+            if sue[item] > count[c]:
+                match_p2 += 1
+            if sue[item] == count[c]:
+                match_p1 += 1
+        else:
+            if sue[item] == count[c]:
+                match_p1 += 1
+                match_p2 += 1
+
+    if match_p1 == 3:
+        p1 = aunt
+    if match_p2 == 3:
+        p2 = aunt
+
+print(f"Part 1: {p1}")
 
 # PART 2
 
-
-print(f"Part 2: {total}")
+print(f"Part 2: {p2}")
