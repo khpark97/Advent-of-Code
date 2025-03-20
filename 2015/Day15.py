0ef8b1eb118128e97db616220811612749753cae
Kihyun Park <khpark970@gmail.com>
2025-03-19 22:24:19 -0700

    2015 Day 15

--- 2015/Day15.py
+++ 2015/Day15.py
@@ -5,11 +5,34 @@ lines = data.splitlines()
 
 # PART 1
 
-total = 0
+import re
+from math import prod
 
-print(f"Part 1: {total}")
+ingredients = {}
+for l in lines:
+    ingredients[l.split()[0]] = [int(x) for x in re.findall(r'-?\d+', l)]
+
+def calculate(teaspoons, part):
+    properties = [0 for _ in range(5)]
+    for i, ing in enumerate(ingredients):
+        for j in range(len(ingredients[ing])):
+            properties[j] += ingredients[ing][j] * teaspoons[i]
+    score = prod([p if p > 0 else 0 for p in properties][:4])
+    if part == 'a':
+        return score
+    if part == 'b':
+        return score if properties[4] == 500 else 0
+
+p1, p2 = 0, 0
+for i in range(101):
+    for j in range(101 - i):
+        for k in range(101 - i - j):
+            l = 100 - i - j - k
+            p1 = max(p1, calculate([i, j, k, l], 'a'))
+            p2 = max(p2, calculate([i, j, k, l], 'b'))
+
+print(f"Part 1: {p1}")
 
 # PART 2
 
-
-print(f"Part 2: {total}")
+print(f"Part 2: {p2}")
