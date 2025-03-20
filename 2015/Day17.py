0635075fc6c8eced057882be0eeb6f095b5369a7
Kihyun Park <khpark970@gmail.com>
2025-03-20 13:09:42 -0700

    2015 Day 17

--- 2015/Day17.py
+++ 2015/Day17.py
@@ -5,11 +5,25 @@ lines = data.splitlines()
 
 # PART 1
 
-total = 0
+from itertools import combinations
 
-print(f"Part 1: {total}")
+containers = [int(x) for x in lines]
+liters = 150
+
+p1, p2 = 0, 0
+low = float('inf')
+for i in range(len(containers)):
+    minimum = 0
+    for c in combinations(containers, i):
+        if sum(c) == liters:
+            low = min(low, i)
+            minimum += 1
+    if p2 == 0 and low != float('inf'):
+        p2 = minimum
+    p1 += minimum
+
+print(f"Part 1: {p1}")
 
 # PART 2
 
-
-print(f"Part 2: {total}")
+print(f"Part 2: {p2}")
