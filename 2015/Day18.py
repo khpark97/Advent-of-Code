886c1af8c2e41a381e507b7366ab41c0f980bebf
Kihyun Park <khpark970@gmail.com>
2025-03-21 11:57:59 -0700

    2015 Day 18

--- 2015/Day18.py
+++ 2015/Day18.py
@@ -5,11 +5,61 @@ lines = data.splitlines()
 
 # PART 1
 
-total = 0
+import re
 
-print(f"Part 1: {total}")
+lights_p1 = [line for line in lines]
+rows, cols = len(lights_p1), len(lights_p1[0])
+
+def toggle(config, part):
+    step = []
+    for r in range(rows):
+        row = []
+        for c in range(cols):
+            #logic to have each corner stay on for part 2
+            if part == '2':
+                if ((r == 0 and c == 0) or
+                    (r == rows-1 and c == 0) or
+                    (r == 0 and c == cols-1) or
+                    (r == rows-1 and c == cols-1)):
+                    row.append('#')
+                    continue
+
+            curr = config[r][c]
+            neis = 0
+            for dr, dc in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+                nr, nc = tuple(map(sum, zip((r, c), (dr, dc))))
+                if nr in range(rows) and nc in range(cols):
+                    if config[nr][nc] == '#':
+                        neis += 1
+            if curr == '#' and neis in [2, 3]:
+                row.append('#')
+            elif curr == '.' and neis == 3:
+                row.append('#')
+            else:
+                row.append('.')
+        step.append(''.join(row))
+    return step
+
+for _ in range(100):
+    lights_p1 = toggle(lights_p1, '1')
+
+total_p1 = 0
+for r in lights_p1:
+    total_p1 += len(re.findall('#', r))
+
+print(f"Part 1: {total_p1}")
 
 # PART 2
 
+lights_p2 = [line for line in lines]
+lights_p2[0] = '#' + lights_p2[0][1:-1] + '#'
+lights_p2[-1] = '#' + lights_p2[-1][1:-1] + '#'
 
-print(f"Part 2: {total}")
+for _ in range(100):
+    lights_p2 = toggle(lights_p2, '2')
+
+total_p2 = 0
+for r in lights_p2:
+    total_p2 += len(re.findall('#', r))
+
+print(f"Part 2: {total_p2}")
