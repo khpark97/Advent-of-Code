196a1cc387af683591c08295f6c7ada888836932
Kihyun Park <khpark970@gmail.com>
2025-03-21 12:36:10 -0700

    2015 Day 19 part 1

--- 2015/Day19.py
+++ 2015/Day19.py
@@ -5,11 +5,31 @@ lines = data.splitlines()
 
 # PART 1
 
-total = 0
+from collections import defaultdict
 
-print(f"Part 1: {total}")
+replacements = defaultdict(list)
+i = 0
+while lines[i]:
+    m, r = lines[i].split(' => ')
+    replacements[m].append(r)
+    i += 1
+
+distinct = set()
+molecule = lines[-1]
+
+for j in range(len(molecule)):
+    check = [molecule[j]]
+    if j < len(molecule)-1:
+        check.append(molecule[j:j+2])
+    for c in check:
+        if c in replacements:
+            for r in replacements[c]:
+                distinct.add(''.join([molecule[:j], r, molecule[j+len(c):]]).strip())    
+
+print(f"Part 1: {len(distinct)}")
 
 # PART 2
 
 
-print(f"Part 2: {total}")
+
+print(f"Part 2: {len(distinct)}")
