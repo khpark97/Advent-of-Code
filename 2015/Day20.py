e9c20b387e554479d28a5fc1c03679945a0a45a4
Kihyun Park <khpark970@gmail.com>
2025-03-22 19:34:08 -0700

    2015 Day 20

--- 2015/Day20.py
+++ 2015/Day20.py
@@ -1,15 +1,36 @@
 ## STARTER CODE
 file = open('2015/Day20_data.txt', 'r')
 data = file.read()
-lines = data.splitlines()
 
 # PART 1
 
-total = 0
+import math
 
-print(f"Part 1: {total}")
+presents = int(data)
+
+n = presents
+houses = [0 for _ in range(n//10 + 1)]
+for i in range(1, n//10 + 1):
+    for j in range(i, n//10 + 1, i):
+        houses[j] += i * 10
+
+for j in range(len(houses)):
+    if houses[j] > presents:
+        house = j
+        break
+
+print(f"Part 1: {house}")
 
 # PART 2
 
+houses = [0 for _ in range(n//10 + 1)]
+for i in range(1, n//10 + 1):
+    for j in range(i, min(n//10 + 1, i * 50 + 1) , i):
+        houses[j] += i * 10
 
-print(f"Part 2: {total}")
+for j in range(len(houses)):
+    if houses[j] > presents:
+        house = j
+        break
+
+print(f"Part 2: {house}")
