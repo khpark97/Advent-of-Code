f261953b827cf92ded35afbb5dc19f8de038ab65
Kihyun Park <khpark970@gmail.com>
2025-03-22 19:41:07 -0700

    2015 Day 20

--- 2015/Day20.py
+++ 2015/Day20.py
@@ -7,31 +7,21 @@ data = file.read()
 import math
 
 presents = int(data)
+houses_p1 = [0 for _ in range(presents//10 + 1)]
+houses_p2 = [0 for _ in range(presents//10 + 1)]
 
-n = presents
-houses = [0 for _ in range(n//10 + 1)]
-for i in range(1, n//10 + 1):
-    for j in range(i, n//10 + 1, i):
-        houses[j] += i * 10
+for i in range(1, presents//10 + 1):
+    for j in range(i, presents//10 + 1, i):
+        houses_p1[j] += i * 10
+    for k in range(i, min(presents//10 + 1, i * 50 + 1) , i):
+        houses_p2[k] += i * 11
 
-for j in range(len(houses)):
-    if houses[j] > presents:
-        house = j
-        break
+p1 = [h for h, house in enumerate(houses_p1) if house > presents][0]
 
-print(f"Part 1: {house}")
+print(f"Part 1: {p1}")
 
 # PART 2
 
-houses = [0 for _ in range(n//10 + 1)]
-for i in range(1, n//10 + 1):
-    for j in range(i, min(n//10 + 1, i * 50 + 1) , i):
-        houses[j] += i * 10
+p2 = [h for h, house in enumerate(houses_p2) if house > presents][0]
 
-for j in range(len(houses)):
-    if houses[j] > presents:
-        house = j
-        break
-    
-
-print(f"Part 2: {house}")
+print(f"Part 2: {p2}")
