0c8da7532bfa7bb5fabb3bbe635f07d6a63d6277
Kihyun Park <khpark970@gmail.com>
2025-03-25 22:35:24 -0700

    2015 Day 24

--- 2015/Day24.py
+++ 2015/Day24.py
@@ -1,15 +1,27 @@
 ## STARTER CODE
 file = open('2015/Day24_data.txt', 'r')
 data = file.read()
-lines = data.splitlines()
 
 # PART 1
 
-total = 0
+from itertools import combinations
+import math
 
-print(f"Part 1: {total}")
+presents = [int(p) for p in data.splitlines()]
+
+def entanglement(n):
+    weight = sum(presents) // n
+    quantum, smallest = float('inf'), float('inf')
+    for i in range(len(presents)): 
+            #range(len(presents) // n + 2) works much faster but i dont know why
+        for c in combinations(presents, i):
+            if len(c) <= smallest and sum(c) == weight:
+                smallest = len(c)
+                quantum = min(math.prod(c), quantum)
+    return quantum
+
+print(f"Part 1: {entanglement(3)}")
 
 # PART 2
 
-
-print(f"Part 2: {total}")
+print(f"Part 2: {entanglement(4)}")
