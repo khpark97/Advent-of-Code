302bbb989dae317c3c2b8423b6ddbe962057e510
Kihyun Park <khpark970@gmail.com>
2025-03-26 11:00:50 -0700

    2015 Day 25 part 1

--- 2015/Day25.py
+++ 2015/Day25.py
@@ -1,15 +1,29 @@
 ## STARTER CODE
 file = open('2015/Day25_data.txt', 'r')
 data = file.read()
-lines = data.splitlines()
 
 # PART 1
 
-total = 0
+import re
+row, col = [int(x) for x in re.findall(r'\d+', data)]
 
-print(f"Part 1: {total}")
+def calculate(r, c):
+    start = 20151125
+    cr, cc = 1, 1
+    while True:
+        if cr == r and cc == c:
+            return start
+        if cr == 1:
+            cr = cc + 1
+            cc = 1
+        else:
+            cr -= 1
+            cc += 1
+        start *= 252533
+        start %= 33554393
+
+print(f"Part 1: {calculate(row, col)}")
 
 # PART 2
 
-
-print(f"Part 2: {total}")
+# print(f"Part 2: {calculate(row, col)}")
