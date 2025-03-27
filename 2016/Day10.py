98bda50f8cbe336c1c086fe2938f357c7c7ff73c
Kihyun Park <khpark970@gmail.com>
2025-03-27 12:06:44 -0700

    setup

---
+++ 2016/Day10.py
@@ -0,0 +1,16 @@
+## STARTER CODE
+file = open('2016/Day10_data.txt', 'r')
+data = file.read()
+lines = data.splitlines()
+
+# PART 1
+
+
+
+print(f"Part 1: {score}")
+
+# PART 2
+
+
+
+print(f"Part 2: {score}")
