0672bc573087b99c8996e0b9f8bf8c1558250c0f
Kihyun Park <khpark970@gmail.com>
2025-03-28 11:42:30 -0700

    2016 Day 6

--- 2016/Day06.py
+++ 2016/Day06.py
@@ -5,12 +5,21 @@ lines = data.splitlines()
 
 # PART 1
 
+from collections import defaultdict, Counter
+chars = defaultdict(list)
+for l in lines:
+    for i in range(len(l)):
+        chars[i].append(l[i])
 
+most = []
+least = []
+for col in chars:
+    count = Counter(chars[col])
+    most.append(count.most_common(1)[0][0])
+    least.append(count.most_common()[-1][0])
 
-print(f"Part 1: {score}")
+print(f"Part 1: {''.join(most)}")
 
 # PART 2
 
-
-
-print(f"Part 2: {score}")
+print(f"Part 2: {''.join(least)}")
