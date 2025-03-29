37e54cc6aac597ee02080884bc9710440e36b941
Kihyun Park <khpark970@gmail.com>
2025-03-29 11:47:34 -0700

    2016 Day 7

--- 2016/Day07.py
+++ 2016/Day07.py
@@ -5,12 +5,40 @@ lines = data.splitlines()
 
 # PART 1
 
+def tls_scan(text):
+    for i in range(len(text) - 3):
+        if text[i] == text[i+3] and text[i+1] == text[i+2] and text[i] != text[i+1]:
+            return True
+    return False
 
+def ssl_scan(texts):
+    res = []
+    for text in texts:
+        for i in range(len(text) - 2):
+            if text[i] == text[i+2] and text[i] != text[i+1]:
+                res.append(''.join([text[i+1], text[i], text[i+1]]))
+    return res
 
-print(f"Part 1: {score}")
+tls, ssl = 0, 0
+for l in lines:
+    split = l.split('[')
+    outside = [split[0]]
+    brackets = []
+    for s in split[1:]:
+        left, right = s.split(']')
+        outside.append(right)
+        brackets.append(left)
+    
+    if not any([tls_scan(b) for b in brackets]):
+        if any([tls_scan(o) for o in outside]):
+            tls += 1
+
+    valid = ssl_scan(outside)
+    if any([v in ''.join(brackets) for v in valid]):
+        ssl += 1
+
+print(f"Part 1: {tls}")
 
 # PART 2
 
-
-
-print(f"Part 2: {score}")
+print(f"Part 2: {ssl}")
