ae908383c408e12b66f5cc7d140627fa88d9a6d0
Kihyun Park <khpark970@gmail.com>
2025-03-30 22:56:04 -0700

    2016 Day 10

--- 2016/Day10.py
+++ 2016/Day10.py
@@ -5,12 +5,42 @@ lines = data.splitlines()
 
 # PART 1
 
+from collections import defaultdict, deque
+import re
+bots = defaultdict(list)
+values = [l for l in lines if l.split()[0] == 'value']
+for v in values:
+    chip, bot = [int(x) for x in re.findall(r"\d+", v)]
+    bots[bot].append(chip)
 
+part_1 = 0
 
-print(f"Part 1: {score}")
+output = defaultdict(int)
+commands = [l for l in lines if l.split()[0] == 'bot']
+q = deque(commands)
+
+while q:
+    curr = q.popleft()
+    bot, first, second = [int(x) for x in re.findall(r"\d+", curr)]
+    if len(bots[bot]) != 2:
+        q.append(curr)
+        continue
+    if sorted(bots[bot]) == [17, 61]:
+        part_1 = bot
+    split = curr.split()
+    if split[5] == 'bot':
+        bots[first].append(min(bots[bot]))
+    elif split[5] == 'output':
+        output[first] = min(bots[bot])
+    if split[10] == 'bot':
+        bots[second].append(max(bots[bot]))
+    elif split[10] == 'output':
+        output[second] = max(bots[bot])
+    # bots[bot] = []
+    
+print(f"Part 1: {part_1}")
 
 # PART 2
 
-
-
-print(f"Part 2: {score}")
+import math
+print(f"Part 2: {math.prod([output[i] for i in range(3)])}")
