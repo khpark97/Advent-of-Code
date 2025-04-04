c890780e7f6a5aa5d18357aef4eaea6ea703c8f1
Kihyun Park <khpark970@gmail.com>
2025-04-04 11:51:55 -0700

    2016 Day 18

--- 2016/Day18.py
+++ 2016/Day18.py
@@ -1,16 +1,38 @@
 ## STARTER CODE
 file = open('2016/Day18_data.txt', 'r')
-data = file.read()
-lines = data.splitlines()
+start = file.read()
 
 # PART 1
 
+def lengthen(n):
+    rows = [start]
+    k = 0
+    while len(rows) < n:
+        curr = ''.join(['.', rows[k], '.'])
+        next = []
+        for i in range(len(curr) - 2):
+            l, c, r = curr[i], curr[i+1], curr[i+2]
+            if l == '^':
+                if r == '.':
+                    next.append('^')
+                else:
+                    next.append('.')
+            elif r == '^':
+                next.append('^')
+            else:
+                next.append('.')
+            
+        rows.append(''.join(next))
+        k += 1
+    return rows
 
+import re
+tiles_p1 = sum([len(re.findall(r'\.', row)) for row in lengthen(40)])
 
-print(f"Part 1: {score}")
+print(f"Part 1: {tiles_p1}")
 
 # PART 2
 
+tiles_p2 = sum([len(re.findall(r'\.', row)) for row in lengthen(400000)])
 
-
-print(f"Part 2: {score}")
+print(f"Part 2: {tiles_p2}")
