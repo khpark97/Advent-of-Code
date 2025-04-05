d83aff2c71d6acf209869b9813f91414edca9357
Kihyun Park <khpark970@gmail.com>
2025-04-05 14:31:05 -0700

    2019 Day 19

--- 2016/Day19.py
+++ 2016/Day19.py
@@ -1,16 +1,33 @@
 ## STARTER CODE
 file = open('2016/Day19_data.txt', 'r')
-data = file.read()
-lines = data.splitlines()
 
 # PART 1
 
+elves = int(file.read())
 
+presents = {elf: 1 for elf in range(1, elves + 1)}
+curr, next = 1, 2
+while True:
+    next = curr % elves + 1
+    while presents[next] == 0:
+        next = next % elves + 1
 
-print(f"Part 1: {score}")
+    presents[curr] += presents[next]
+    presents[next] = 0
+
+    if presents[curr] == elves:
+        break
+    
+    curr = next % elves + 1
+    while presents[curr] == 0:
+        curr = curr % elves + 1
+
+print(f"Part 1: {curr}")
 
 # PART 2
 
+i = 1
+while i * 3 < elves:
+    i *= 3
 
-
-print(f"Part 2: {score}")
+print(f"Part 2: {elves - i}")
