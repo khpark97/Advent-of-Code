127658b9674c6d298c91cfda5ec92b24f57060ca
Kihyun Park <khpark970@gmail.com>
2025-04-06 11:05:10 -0700

    2016 Day 20

--- 2016/Day20.py
+++ 2016/Day20.py
@@ -5,12 +5,22 @@ lines = data.splitlines()
 
 # PART 1
 
+import re
+ranges = [(int(start), int(end)) for start, end in [re.findall(r'\d+', l) for l in lines]]
 
+curr, lowest = 0, float('inf')
+allowed = 0
+for r in sorted(ranges):
+    start, end = r
+    if curr >= start:
+        curr = max(curr, end + 1)
+    else:
+        lowest = min(lowest, curr)
+        allowed += start - curr
+        curr = end + 1
 
-print(f"Part 1: {score}")
+print(f"Part 1: {lowest}")
 
 # PART 2
 
-
-
-print(f"Part 2: {score}")
+print(f"Part 2: {allowed}")
