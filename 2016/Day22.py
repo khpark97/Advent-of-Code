845ea7d0dea3c7ea547ede0fb4a39b6c6494a0a9
Kihyun Park <khpark970@gmail.com>
2025-04-09 12:21:23 -0700

    2016 Day 22 part 1

--- 2016/Day22.py
+++ 2016/Day22.py
@@ -5,12 +5,24 @@ lines = data.splitlines()
 
 # PART 1
 
+import re
+all_nodes = []
+for l in lines[2:]:
+    x, y, size, used, avail, percentage = [int(x) for x in re.findall(r'\d+', l)]
+    all_nodes.append((x, y, size, used, avail))
 
+viable = 0
+for i in range(len(all_nodes)):
+    used = all_nodes[i][3]
+    if used != 0:
+        for j in range(len(all_nodes)):
+            if i != j:
+                avail = all_nodes[j][4]
+                if avail >= used:
+                    viable += 1
 
-print(f"Part 1: {score}")
+print(f"Part 1: {viable}")
 
 # PART 2
 
-
-
-print(f"Part 2: {score}")
+print(f"Part 2: {viable}")
