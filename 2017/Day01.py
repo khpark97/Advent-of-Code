c5a97d92eabb5cffdec533efa944834bfce4b707
Kihyun Park <khpark970@gmail.com>
2025-04-14 10:39:05 -0700

    2017 Day 1

--- 2017/Day01.py
+++ 2017/Day01.py
@@ -1,13 +1,25 @@
 ## STARTER CODE
-file = open('2017/Day_data.txt', 'r')
+file = open('2017/Day01_data.txt', 'r')
 data = file.read()
-lines = data.splitlines()
 
 # PART 1
 
-score = 0
+digits = [int(x) for x in data]
+score = digits[0] if digits[0] == digits[-1] else 0
+i = 0
+while i < len(digits) - 1:
+    if digits[i] == digits[i+1]:
+        score += digits[i]
+    i += 1
+
 print(f"Part 1: {score}")
 
 # PART 2
+
+score = 0
+halfway = int(len(digits) / 2)
+for i in range(halfway):
+    if digits[i] == digits[i + halfway]:
+        score += digits[i] * 2
 
 print(f"Part 2: {score}")
