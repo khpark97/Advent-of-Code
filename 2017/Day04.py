33b7776904880f77ae535ac46eac5ff1b2230918
Kihyun Park <khpark970@gmail.com>
2025-04-14 10:27:49 -0700

    2017 setup

---
+++ 2017/Day04.py
@@ -0,0 +1,13 @@
+## STARTER CODE
+file = open('2017/Day04_data.txt', 'r')
+data = file.read()
+lines = data.splitlines()
+
+# PART 1
+
+score = 0
+print(f"Part 1: {score}")
+
+# PART 2
+
+print(f"Part 2: {score}")
