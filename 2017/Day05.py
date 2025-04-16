6fc6b1ee2381c0d17bf90d8388fe11635a3759f2
Kihyun Park <khpark970@gmail.com>
2025-04-16 15:19:21 -0700

    2017 Day 5

--- 2017/Day05.py
+++ 2017/Day05.py
@@ -5,9 +5,28 @@ lines = data.splitlines()
 
 # PART 1
 
-score = 0
-print(f"Part 1: {score}")
+instructions = [int(x) for x in lines]
+steps = 0
+pointer = 0
+while pointer < len(instructions):
+    instructions[pointer] += 1
+    pointer += instructions[pointer] - 1
+    steps += 1
+
+print(f"Part 1: {steps}")
 
 # PART 2
 
-print(f"Part 2: {score}")
+instructions = [int(x) for x in lines]
+steps = 0
+pointer = 0
+while pointer < len(instructions):
+    offset = instructions[pointer]
+    if instructions[pointer] >= 3:
+        instructions[pointer] -= 1
+    else:
+        instructions[pointer] += 1
+    pointer += offset
+    steps += 1
+
+print(f"Part 2: {steps}")
