a6f13b9e7679b8153a57c2b1c3cae80f916eeb2a
Kihyun Park <khpark970@gmail.com>
2025-04-16 15:12:31 -0700

    2017 Day 4

--- 2017/Day04.py
+++ 2017/Day04.py
@@ -5,9 +5,25 @@ lines = data.splitlines()
 
 # PART 1
 
-score = 0
-print(f"Part 1: {score}")
+from collections import Counter
+
+valid_p1, valid_p2 = 0, 0
+for l in lines:
+    split = l.split()
+    most = Counter(split).most_common(1).pop()
+    if most[1] == 1:
+        valid_p1 += 1
+
+    anagram = False
+    for i in range(len(split)):
+        count = Counter(split[i])
+        for j in range(i+1, len(split)):
+            if Counter(split[j]) == count:
+                anagram = True
+    valid_p2 += 0 if anagram else 1
+
+print(f"Part 1: {valid_p1}")
 
 # PART 2
 
-print(f"Part 2: {score}")
+print(f"Part 2: {valid_p2}")
