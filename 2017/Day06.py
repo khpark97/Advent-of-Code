b089e3e0558df5c464f649b6649032afd3d82275
Kihyun Park <khpark970@gmail.com>
2025-04-17 10:53:05 -0700

    2017 Day 6

--- 2017/Day06.py
+++ 2017/Day06.py
@@ -1,13 +1,31 @@
 ## STARTER CODE
 file = open('2017/Day06_data.txt', 'r')
 data = file.read()
-lines = data.splitlines()
 
 # PART 1
 
-score = 0
-print(f"Part 1: {score}")
+banks = [int(x) for x in data.split()]
+seen = {tuple(banks): 0}
+
+def redistribute(config):
+    most = max(config)
+    index = config.index(most)
+    config[index] = 0
+    for bank in range(index+1, index+1+most):
+        i = bank % len(config)
+        config[i] += 1
+    return config
+
+cycles = 0
+while True:
+    banks = redistribute(banks)
+    cycles += 1
+    if tuple(banks) in seen:
+        break
+    seen[tuple(banks)] = cycles
+
+print(f"Part 1: {cycles}")
 
 # PART 2
 
-print(f"Part 2: {score}")
+print(f"Part 2: {cycles - seen[tuple(banks)]}")
