86237023b65c85e19429c800cb6bfdbc36effc62
Kihyun Park <khpark970@gmail.com>
2025-04-19 10:07:54 -0700

    2017 Day 8

--- 2017/Day08.py
+++ 2017/Day08.py
@@ -5,9 +5,26 @@ lines = data.splitlines()
 
 # PART 1
 
-score = 0
-print(f"Part 1: {score}")
+registers = {}
+highest = -1
+for l in lines:
+    r, operand, amount, _, *condition = l.split()
+    con_r = condition[0]
+    if con_r not in registers:
+        registers[con_r] = 0
+    condition[0] = registers[con_r]
+    if r not in registers:
+        registers[r] = 0
+    if eval(''.join([str(x) for x in condition])):
+        match operand:
+            case 'inc':
+                registers[r] += int(amount)
+            case 'dec':
+                registers[r] -= int(amount)
+    highest = max(highest, max(registers.values()))
+
+print(f"Part 1: {max(registers.values())}")
 
 # PART 2
 
-print(f"Part 2: {score}")
+print(f"Part 2: {highest}")
