d2568d7759ca9f1f0465569c8a4916d03d08b3f4
Kihyun Park <khpark970@gmail.com>
2025-04-21 14:15:06 -0700

    2017 Day 10 part 1

--- 2017/Day10.py
+++ 2017/Day10.py
@@ -1,12 +1,36 @@
 ## STARTER CODE
 file = open('2017/Day10_data.txt', 'r')
 data = file.read()
-lines = data.splitlines()
 
 # PART 1
 
+import re
+lengths = [int(x) for x in re.findall(r'\d+', data)]
+
+elements = [e for e in range(256)]
+
+pos, skip = 0, 0
+for l in lengths:
+    leftovers = 0
+    curr = elements[pos:min(pos+l, len(elements))]
+    if pos + l > len(elements):
+        # Loops back around to the elements in the front
+        leftovers = (pos + l) % len(elements)
+        curr += elements[:leftovers]
+
+    curr.reverse()
+    for i in range(pos, min(pos+l, len(elements))):
+        elements[i] = curr[i - pos]
+        
+    i += 1
+    for j in range(leftovers):
+        elements[j] = curr[(i - pos) + j]
+
+    pos = (pos + l + skip) % len(elements)
+    skip += 1
+
 score = 0
-print(f"Part 1: {score}")
+print(f"Part 1: {elements[0] * elements[1]}")
 
 # PART 2
 
