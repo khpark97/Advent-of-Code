04f1bdbecdc7a1a89ce7e724b82c755f966aea59
Kihyun Park <khpark970@gmail.com>
2025-04-22 11:31:27 -0700

    2017 Day 10

--- 2017/Day10.py
+++ 2017/Day10.py
@@ -7,31 +7,52 @@ data = file.read()
 import re
 lengths = [int(x) for x in re.findall(r'\d+', data)]
 
-elements = [e for e in range(256)]
+input = [e for e in range(256)]
 
 pos, skip = 0, 0
-for l in lengths:
-    leftovers = 0
-    curr = elements[pos:min(pos+l, len(elements))]
-    if pos + l > len(elements):
-        # Loops back around to the elements in the front
-        leftovers = (pos + l) % len(elements)
-        curr += elements[:leftovers]
+def knot(elements, lengths):
+    global pos, skip
+    for l in lengths:
+        leftovers = 0
+        curr = elements[pos:min(pos+l, len(elements))]
+        if pos + l > len(elements):
+            # Loops back around to the elements in the front
+            leftovers = (pos + l) % len(elements)
+            curr += elements[:leftovers]
 
-    curr.reverse()
-    for i in range(pos, min(pos+l, len(elements))):
-        elements[i] = curr[i - pos]
-        
-    i += 1
-    for j in range(leftovers):
-        elements[j] = curr[(i - pos) + j]
+        curr.reverse()
+        for i in range(pos, min(pos+l, len(elements))):
+            elements[i] = curr[i - pos]
+            
+        i += 1
+        for j in range(leftovers):
+            elements[j] = curr[(i - pos) + j]
 
-    pos = (pos + l + skip) % len(elements)
-    skip += 1
+        pos = (pos + l + skip) % len(elements)
+        skip += 1
+    return elements
 
-score = 0
-print(f"Part 1: {elements[0] * elements[1]}")
+input = knot(input, lengths)
+
+print(f"Part 1: {input[0] * input[1]}")
 
 # PART 2
 
-print(f"Part 2: {score}")
+lengths = [ord(char) for char in data] + [17, 31, 73, 47, 23]
+
+pos, skip = 0, 0
+input = [e for e in range(256)]
+for _ in range(64):
+    input = knot(input, lengths)
+
+dense = []
+for i in range(16):
+    section = input[i*16:(i+1)*16]
+    res = section[0]
+    for j in section[1:]:
+        res ^= j
+    dense.append(res)
+
+result = ''.join([f"{x:02x}" for x in dense])
+
+print(f"Part 2: {result}")
