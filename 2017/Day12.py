62af57fd568d6a4785b9fe0cb02cec7fb73ee1ca
Kihyun Park <khpark970@gmail.com>
2025-04-24 08:34:32 -0700

    2017 Day 12

--- 2017/Day12.py
+++ 2017/Day12.py
@@ -5,9 +5,34 @@ lines = data.splitlines()
 
 # PART 1
 
-score = 0
-print(f"Part 1: {score}")
+import re
+connections = {}
+for line in lines:
+    program, *connected = [int(x) for x in re.findall(r'\d+', line)]
+    connections[program] = connected
+
+from collections import deque
+def find_group(id):
+    q = deque([id])
+    seen = set([id])
+    while q:
+        curr = q.popleft()
+        for connected in connections[curr]:
+            if connected not in seen:
+                seen.add(connected)
+                q.append(connected)
+    return seen
+
+group = find_group(0)
+
+print(f"Part 1: {len(group)}")
 
 # PART 2
 
-print(f"Part 2: {score}")
+groups = 1
+for c in connections:
+    if c not in group:
+        group.update(find_group(c))
+        groups += 1
+
+print(f"Part 2: {groups}")
