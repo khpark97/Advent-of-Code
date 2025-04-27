7d540ea545619b4691a0930c61512f94075cccbe
Kihyun Park <khpark970@gmail.com>
2025-04-27 10:49:45 -0700

    2017 Day 14

--- 2017/Day10.py
+++ 2017/Day10.py
@@ -5,13 +5,8 @@ data = file.read()
 # PART 1
 
 import re
-lengths = [int(x) for x in re.findall(r'\d+', data)]
 
-input = [e for e in range(256)]
-
-pos, skip = 0, 0
-def knot(elements, lengths):
-    global pos, skip
+def knot_p1(elements, lengths, pos, skip):
     for l in lengths:
         leftovers = 0
         curr = elements[pos:min(pos+l, len(elements))]
@@ -30,29 +25,37 @@ def knot(elements, lengths):
 
         pos = (pos + l + skip) % len(elements)
         skip += 1
-    return elements
-
-input = knot(input, lengths)
-
-print(f"Part 1: {input[0] * input[1]}")
+    return elements, pos, skip
 
 # PART 2
 
-lengths = [ord(char) for char in data] + [17, 31, 73, 47, 23]
+def knot_p2(key):
+    lengths = [ord(char) for char in key] + [17, 31, 73, 47, 23]
 
-pos, skip = 0, 0
-input = [e for e in range(256)]
-for _ in range(64):
-    input = knot(input, lengths)
+    pos, skip = 0, 0
+    input = [e for e in range(256)]
+    for _ in range(64):
+        res = knot_p1(input, lengths, pos, skip)
+        input = res[0]
+        pos, skip = res[1:]
 
-dense = []
-for i in range(16):
-    section = input[i*16:(i+1)*16]
-    res = section[0]
-    for j in section[1:]:
-        res ^= j
-    dense.append(res)
+    dense = []
+    for i in range(16):
+        section = input[i*16:(i+1)*16]
+        res = section[0]
+        for j in section[1:]:
+            res ^= j
+        dense.append(res)
 
-result = ''.join([f"{x:02x}" for x in dense])
+    return ''.join([f"{x:02x}" for x in dense])
 
-print(f"Part 2: {result}")
+if __name__ == "__main__":
+    lengths = [int(x) for x in re.findall(r'\d+', data)]
+
+    input = [e for e in range(256)]
+    
+    input = knot_p1(input, lengths, 0, 0)[0]
+
+    print(f"Part 1: {input[0] * input[1]}")
+
+    print(f"Part 2: {knot_p2(data)}")
--- 2017/Day14.py
+++ 2017/Day14.py
@@ -1,13 +1,44 @@
 ## STARTER CODE
 file = open('2017/Day14_data.txt', 'r')
-data = file.read()
-lines = data.splitlines()
+key = file.read()
+
+from Day10 import knot_p2
 
 # PART 1
 
+inputs = [key + '-' + str(i) for i in range(128)]
+grid = []
+for i in inputs:
+    hash = knot_p2(i)
+    grid.append(f"{int(hash, 16):0128b}")
+
 score = 0
+for row in grid:
+    ints = [int(bit) for bit in row]
+    score += sum(ints)
+
 print(f"Part 1: {score}")
 
 # PART 2
 
-print(f"Part 2: {score}")
+from collections import deque
+ones = [(r, c) for r in range(len(grid)) for c in range(len(grid[0])) if grid[r][c] == '1']
+seen = set()
+groups = 0
+dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+q = deque()
+for i in range(len(ones)):
+    if ones[i] not in seen:
+        q.append(ones[i])
+        groups += 1
+    while q:
+        curr = q.popleft()
+        seen.add(curr)
+        for d in dirs:
+            nr, nc = tuple(map(sum, zip(curr, d)))
+            if nr in range(len(grid)) and nc in range(len(grid[0])):
+                if (nr, nc) not in seen:
+                    if grid[nr][nc] == '1':
+                        q.append((nr, nc))
+
+print(f"Part 2: {groups}")
