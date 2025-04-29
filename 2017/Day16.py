9ae4a682f6d5160f18c0b48d8eb22670a59adff5
Kihyun Park <khpark970@gmail.com>
2025-04-29 12:15:12 -0700

    2017 Day 16 part 1

--- 2017/Day16.py
+++ 2017/Day16.py
@@ -1,13 +1,36 @@
 ## STARTER CODE
 file = open('2017/Day16_data.txt', 'r')
 data = file.read()
-lines = data.splitlines()
 
 # PART 1
 
-score = 0
-print(f"Part 1: {score}")
+import string
+moves = data.split(',')
+programs = [char for char in string.ascii_lowercase[:16]]
+
+def dance(order):
+    for move in moves:
+        match move[0]:
+            case 's': 
+                spin = int(move[1:])
+                order = order[-spin:] + order[:-spin]
+            case 'x':
+                p1, p2 = [int(x) for x in move[1:].split('/')]
+                temp = order[p1]
+                order[p1] = order[p2]
+                order[p2] = temp
+            case 'p':
+                p1, p2 = [order.index(x) for x in move[1:].split('/')]
+                temp = order[p1]
+                order[p1] = order[p2]
+                order[p2] = temp
+    return order
+
+danced = dance(programs.copy())
+
+print(f"Part 1: {''.join(danced)}")
 
 # PART 2
 
-print(f"Part 2: {score}")
+
+print(f"Part 2: {''.join(programs)}")
