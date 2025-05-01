d395649151082e1a79007bfa5799bc2d27e1ea01
Kihyun Park <khpark970@gmail.com>
2025-05-01 12:09:51 -0700

    2017 Day 17

--- 2017/Day17.py
+++ 2017/Day17.py
@@ -1,13 +1,24 @@
 ## STARTER CODE
 file = open('2017/Day17_data.txt', 'r')
-data = file.read()
-lines = data.splitlines()
+steps = int(file.read())
 
 # PART 1
 
-score = 0
-print(f"Part 1: {score}")
+buffer = [0]
+position = 0
+for i in range(1, 2018):
+    position = (position + steps) % i + 1
+    buffer = buffer[:position] + [i] + buffer[position:]
+
+print(f"Part 1: {buffer[position+1]}")
 
 # PART 2
 
-print(f"Part 2: {score}")
+position = 0
+after_zero = 0
+for i in range(1, 50000001):
+    position = (position + steps) % i + 1
+    if position == 1:
+        after_zero = i
+
+print(f"Part 2: {after_zero}")
